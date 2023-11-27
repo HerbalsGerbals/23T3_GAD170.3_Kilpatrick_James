3728ba0ccc358589808eba1749d19891f465ad9c
HerbalsGerbals <145905476+HerbalsGerbals@users.noreply.github.com>
2023-11-27 11:28:20 +1100

    Mechanic 3 working (button press in context)

--- Assets/Scripts/PlayerRigidBody.cs
+++ Assets/Scripts/PlayerRigidBody.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class PlayerRigidBody : MonoBehaviour
 {
+    [SerializeField] private int sceneIndexToLoad;
+
     public Rigidbody body;
     // Start is called before the first frame update
     void Start()
@@ -22,7 +24,7 @@ public class PlayerRigidBody : MonoBehaviour
         if (collision.gameObject.CompareTag("Obstacle"))
         {
             Debug.Log(gameObject.name + " hit " + collision.gameObject.name + "Hit hazard restarting level");
-            SceneManager.LoadScene(1);
+            SceneManager.LoadScene(sceneIndexToLoad);
         }
 
     }    
---
+++ Assets/Scripts/Sunlight.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Sunlight : MonoBehaviour
+{
+    [SerializeField] private GameObject Player;
+    [SerializeField] private int sceneIndexToLoad;
+    [SerializeField] private KeyCode keyToPress;
+    [SerializeField] private bool isPlayerCharacterNearby = false;
+
+    // Want to dectect player character in area
+    private void SunlightReached()
+    {
+        SceneManager.LoadScene(sceneIndexToLoad);
+    }
+    //When Player Character enters area set isPlayerCharacterNearby to true
+    //if isPlayerCharacterNearby = true allow for GetKeyDown(keyToPress) to run
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        Debug.Log("Trigger Activated" + gameObject);
+        isPlayerCharacterNearby = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        isPlayerCharacterNearby = false;
+    }
+    private void Update()
+    {
+        // if Player presses "E" and Player Character boolean is true
+        if (Input.GetKeyDown(keyToPress) && isPlayerCharacterNearby == true)
+        {
+            //Goal reached restart level
+            //E activates code in area
+            Debug.Log("Goal Reached! You pressed E");
+            SunlightReached();
+        }
+    }
+}
